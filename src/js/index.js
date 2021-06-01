d0b3c766a194134552b030c2c88321401b467fcd
MordecaiOhhh <44517579+MordecaiOhhh@users.noreply.github.com>
2021-06-01 17:16:38 +0600

    subscribe is done

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".subscribe-slider", {
+    navigation: {
+        nextEl: ".subscribe-slider-next",
+        prevEl: ".subscribe-slider-prev",
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
